84a903b0f5c6477177856d492ee628a972e8b51a
无涯 <359975715@qq.com>
2024-12-01 19:31:52 +0800

    sui sdk 测试

--- frontend_next/src/app/page.tsx
+++ frontend_next/src/app/page.tsx
@@ -15,6 +15,9 @@ export default function HomePage() {
         <Link href={"/detail/?objectId=1"} className="border p-4 rounded-md bg-pink-600 border-red-600 shadow text-white">
           Go to detail page
         </Link>
+        <Link href={"/callsui/?objectId=1"} className="border p-4 rounded-md bg-pink-600 border-red-600 shadow text-white">
+          Go to callsui page
+        </Link>
       </div>
       <ConnectButton />
     </main>
