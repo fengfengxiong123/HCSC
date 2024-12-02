f484d790477eccac788619d9c4d605a46107767c
Birch <birch17261@gmail.com>
2024-12-02 22:09:23 +0800

    feat(add): home demo page

--- frontend_next/next.config.js
+++ frontend_next/next.config.js
@@ -6,7 +6,8 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-    output: "export"
+  output: "export",
+  images: { unoptimized: true },
 };
 
 export default config;
--- frontend_next/src/app/page.tsx
+++ frontend_next/src/app/page.tsx
@@ -18,6 +18,9 @@ export default function HomePage() {
         <Link href={"/callsui/?objectId=1"} className="border p-4 rounded-md bg-pink-600 border-red-600 shadow text-white">
           Go to callsui page
         </Link>
+        <Link href={"/home_demo/"} className="border p-4 rounded-md bg-pink-600 border-red-600 shadow text-white">
+          Go to home demo page
+        </Link>
       </div>
       <ConnectButton />
     </main>
